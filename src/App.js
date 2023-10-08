4041fcf0a44112dac3463d035fc3c70956170373
vishwas1143 <vishwasburra23143@gmail.com>
2023-10-09 00:34:09 +0530

    PropsinReact

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+
+import React from 'react'; // Import React
+import ExpenseItem from '../src/components/ExpenseItem';
 
 function App() {
+  const expenses = [
+    {
+      id: 'e1',
+      title: 'Toilet Paper',
+      amount: 50,
+      date: new Date(2021, 8, 15),
+    },
+    {
+      id: 'e2',
+      title: 'Tv',
+      amount: 900,
+      date: new Date(2021, 9, 20),
+    },
+    {
+      id: 'e3',
+      title: 'Car',
+      amount: 500,
+      date: new Date(2022, 3, 19),
+    },
+  ];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>Let's Get Started</h2>
+      {/* Use a loop to render ExpenseItem components */}
+      {expenses.map((expense) => (
+        <ExpenseItem
+          key={expense.id} // You should provide a unique key for each list item
+          title={expense.title}
+          amount={expense.amount}
+          date={expense.date}
+        />
+      ))}
     </div>
   );
 }
